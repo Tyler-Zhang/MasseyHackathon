77108424e763af4c8bc25e36888196d75ea947ae
Tyler Zhang <TZTylerzhang@gmail.com>
2016-07-10 13:57:00 -0400

    Completely restructured backend
    Now with every response JSON, there will be a response type = [ERROR/
    SUCCESS], and the body will be either the error message or an
    information payload. Also added instructions on how to run the server,
    and changed create.html to reflect backend changes

--- Server/index.js
+++ Server/index.js
@@ -24,220 +24,192 @@ app.get("/:page", function(req, res){
    res.sendFile(path.join(__dirname, "Website", req.params.page + ".html")); 
 });
 
-// Post request to create room
-app.post("/createroom", function(req,res){
-    var body="";
-	req.on("data",function(data){
-		body+=data;
-		//Check to see if someone is trying to crash the server
-		if(body.length >1e6)
-			request.connection.destroy();
-	});
-
-	req.on("end",function(){
-        // After recieving data
-        var data = JSON.parse(body);
-        console.log(data);
-        // Check which kind of device is trying to connect, if android, automatically add them to the group
-        if(data.type != 'computer' && data.type!= 'android'){
-            res.json(msg(ERR, "NO DEVICE TYPE SENT"));
+app.post("/createroom", (req, res) => {
+    onReq(req, res, (data) => {                                 // res will be in the scope of the function when it is run from within onReq
+        if(!checkData(res, data, ["type", "name"]))
             return;
-        }
+
         var code = genChars(5);
 
-        if(data.type == 'computer'){
-            // If request type is computer
-            var newObj = {};
-           
-            ref.child("/" + code).update({   // Id for the group
-                userAmt: 0,         // Amount of people in the group 
-                 });
-
-            resp(res, RSP,  {grID: code})
-            console.log("Created new room with code: %s Type: %s", code, data.type);
-        } else {
-            // If request type is an android device
+        if(data.type == 'computer'){                            // If the device type is a computer
+            ref.child("/" + code).update({                      // Create the group object on firebase
+                userAmt: 0,                                     // Amount of people in the group 
+            });
+            resp(res, SUC,  {grID: code})
+        } else if(data.type == "android"){                                                // If request type is an android device
             ref.child("/" + code).update({
-                userAmt: 1,     // Amount of people in the group
+                userAmt: 1,
                 users: {
-                    1: {
-                        name: data.name
-                    }
+                    0: {name: data.name}                        // Create the group with one user inplace
                 }
             });
-            resp(res, RSP, {grID: code, id: 1});
-            console.log("Created new room with code: %s Type: %s", code, data.type);        
+            resp(res, SUC, {grID: code, id: 1});
+        } else {
+            resp(res, ERR, "MUST SPECIFY DEVICE AS EITHER COMPUTER OR ANDROID");
+            return;
         }
-    });
+        console.log("Created new room with code: %s Type: %s", code, data.type);     
+    })
 });
 
-app.post("/joinroom", function(req, res){
-    var body="";
-	req.on("data",function(data){
-		body+=data;
-		//Check to see if someone is trying to crash the server
-		if(body.length >1e6)
-			request.connection.destroy();
-	});
+app.post("/joinroom", (req, res) => {
+    onReq(req, res, (data) => {
+        if(!checkData(res, data, ["grID", "name"]))
+            return;
 
-	req.on("end",function(){
-        // After recieving data
-        var data = JSON.parse(body);
-        if(obj.grID.length != 5){
+        if(data.grID.match(/^\w{5}$/) == null)                  // Make sure that they sent id of group they want to join
             resp(res, ERR, "INVALID grID")
-            console.log("Error: tried to join room with invalid code length: %s", obj.grID);
-        }
         else
-            addToRoom(obj, res);
-        
+            addToRoom(data, res);
     });
-    
 });
+
 // Data takes rmID, id, and minutes
 function addToRoom(data, res){
     var func = function(snapshot){
         var obj = snapshot.val();
         console.log(obj);
-        if(obj == null){
-            console.log("Error: Room ID [%s] not found", data.grID);
+        if(obj == null)
+        {
             resp(res, ERR, "grID NOT FOUND");
-        } else {
-             console.log("User [%s] successfully joined  room [%s]", data.name, data.grID);
-            var func = function(snapshot){
-                var usrAmt = snapshot.val();
-                ref.child("/"+data.grID+"/userAmt").set(usrAmt+1);
-                ref.child("/"+data.grID+"/users/" + (usrAmt+1)).update({name: data.name});
-            }
-            ref.child("/"+data.grID+"/userAmt").once("value",func);
-            res.json({id:usrAmt + 1});
+            return;
         }
+        var func = function(snapshot){
+            var usrAmt = snapshot.val();
+            ref.child("/"+data.grID+"/userAmt").set(usrAmt+1);
+            ref.child("/"+data.grID+"/users/" + (usrAmt)).update({name: data.name});
+            resp(res, SUC, {id: usrAmt});
+            console.log("User [%s] successfully joined  room [%s] width id [%d]", data.name, data.grID, usrAmt);
+        }
+        ref.child("/"+data.grID+"/userAmt").once("value",func);
     }
     ref.child("/"+data.grID).once("value", func);
-    
 }
 
-app.post("/report", function(req, res){
-       var body="";
-	req.on("data",function(data){
-		body+=data;
-		//Check to see if someone is trying to crash the server
-		if(body.length >1e6)
-			request.connection.destroy();
-	});
+app.post("/report", (req, res) => {
+    onReq(req, res, (data) => {
+        if(!checkData(res, data, ["grID", "id", "milli"]))
+            return;
 
-	req.on("end",function(){
-       var obj = JSON.parse(body);
-       console.log(obj.time);
-       console.log(new Date().getTime());
-       if(obj.time == null)
-            logTime(obj, new Date(), res);
-       else
-            logTime(obj, new Date(obj.time), res);
-    
+        if(data.time == null)
+            logTime(data, new Date(), res);
+        else
+            logTime(data, new Date(data.time), res);
     });
 });
 
 function logTime(data, date, res){
     
     console.log(data);
-    // Check to make sure information payload is complete
-
-    if(!data.hasOwnProperty("grID") || !data.hasOwnProperty("id") || !data.hasOwnProperty("milli")){
-        console.log("ERROR: NOT SENDING COMPLETE DATA");
-        resp(res, ERR, "LOGTIME REQUIRES ADDITIONAL DATA");
-        
-    } else {
-        console.log(data);
-        var screenStop = date.getTime();
-        var recMinutes = Math.round(data.milli/60000);
-        var currHourMin = 0, lastHourMin = 0;
-        var newRef = ref.child("/" + data.grID +"/users/" + data.id + "/");
-        
-        if(date.getMinutes() >= recMinutes){
-            currHourMin = recMinutes;
-            recMinutes = 0;
-        }
-        else {
-            currHourMin = date.getMinutes();
-            recMinutes -= currHourMin;
-        }
-        
-        if(recMinutes > 0){
-            lastHourMin = recMinutes%60;
-            recMinutes -= lastHourMin;
-        }
-
-        //This code should be optimized by downloading the data first instead of requesting each time
-
-        var screenStart = date.getTime() - data.milli;
-        var startDate = new Date(screenStart);
-        console.log("recMinutes: %d, currHourMin: %d, lastHourMin: %d, ", recMinutes, currHourMin, lastHourMin);
-        newRef.child("/" + date.getMonth() + "/" + date.getDate() + "/" + date.getHours()).once("value", function(snapshot){
-            var oldValue = ((snapshot.val() == null)? 0 : snapshot.val());
-            snapshot.ref.set(oldValue + currHourMin);
-            
-            if(lastHourMin == 0)
-                return;
-
-            newRef.child("/" + startDate.getMonth() + "/" + startDate.getDate() + "/" + startDate.getHours()).once("value", function(snapshot){
-            var oldValue = ((snapshot.val() == null)? 0 : snapshot.val());
-            snapshot.ref.set(oldValue + lastHourMin);
-            startDate.add(1).hours();
-            while(startDate.getTime() < screenStop){
-                newRef.child(startDate.getMonth() + "/" + startDate.getDate() + "/" + startDate.getHours()).set(60);
-                console.log("adding: " + startDate);
-                startDate.add(1).hours();   
-            }
-            
-         });        
-    });     
+    var screenStop = date.getTime();
+    var recMinutes = Math.round(data.milli/60000);
+    var currHourMin = 0, lastHourMin = 0;
+    var newRef = ref.child("/" + data.grID +"/users/" + data.id + "/");
+    
+    if(date.getMinutes() >= recMinutes){
+        currHourMin = recMinutes;
+        recMinutes = 0;
     }
+    else {
+        currHourMin = date.getMinutes();
+        recMinutes -= currHourMin;
+    }
+    
+    if(recMinutes > 0){
+        lastHourMin = recMinutes % 60;
+        recMinutes -= lastHourMin;
+    }
+
+    //This code should be optimized by downloading the data first instead of requesting each time
+    // If we want this to be really bullet proof, it should be recursive to make sure no hour's usage exceeds 60 minutes
+    var screenStart = date.getTime() - data.milli;
+    var startDate = new Date(screenStart);
+    console.log("recMinutes: %d, currHourMin: %d, lastHourMin: %d, ", recMinutes, currHourMin, lastHourMin);
+    newRef.child("/" + date.getMonth() + "/" + date.getDate() + "/" + date.getHours()).once("value", function(snapshot){
+        var oldValue = ((snapshot.val() == null)? 0 : snapshot.val());
+        snapshot.ref.set(oldValue + currHourMin);
+        
+        if(lastHourMin == 0)
+            return;
+
+        newRef.child("/" + startDate.getMonth() + "/" + startDate.getDate() + "/" + startDate.getHours()).once("value", function(snapshot){
+        var oldValue = ((snapshot.val() == null)? 0 : snapshot.val());
+        snapshot.ref.set(oldValue + lastHourMin);
+        startDate.add(1).hours();
+        while(startDate.getTime() < screenStop){
+            newRef.child(startDate.getMonth() + "/" + startDate.getDate() + "/" + startDate.getHours()).set(60);
+            console.log("adding: " + startDate);
+            startDate.add(1).hours();   
+        }         
+        });        
+    });
+    resp(res, SUC, "UPDATED TIME TABLES");
 }
 
-/*  This is to get the data from firebase
-    grID
-    id
-    timeStart
-    timeStop
-*/
+app.post("/view", (req, res) => {
+    onReq(req, res, (data) => {
+        if(!checkData(res, data, ["grID"]))
+            return;
 
-app.post("/view",function(req, res){
+        var newRef = ref.child(data.grID);
+        
+        console.log("Request data group ID: " + data.grID + "/" + data.id);
+        if(!!data.id)
+            newRef = newRef.child(data.id);
+
+        newRef.once("value",function(snapshot){
+            console.log(snapshot.val());
+            resp(res, SUC, snapshot.val());
+        });
+    });
+});
+
+// Create web server
+http.createServer(app).listen(80, function(){
+	console.log("The server has been opened on port 80");
+});
+
+// Network Functions
+
+function onReq(req, res, callBack)
+{
     var body="";
-	req.on("data", function(data){
+	req.on("data",function(data){
 		body+=data;
 		//Check to see if someone is trying to crash the server
 		if(body.length >1e6)
 			request.connection.destroy();
 	});
 
-	req.on("end",function(){
-        var obj = JSON.parse(body);
-        if(!obj.grID)
-        {
-            console.log("ERROR: NO GROUP ID RECIEVED");
-            resp(res, ERR, "NO GROUP ID RECIEVED");
-        }
-
-        console.log("Request data group ID: " + obj.grID);
-        var newRef = ref.child(obj.grID);
-        
-        console.log("Request data group ID: " + obj.grID + "/" + obj.id);
-        if(!!obj.id)
-            newRef = newRef.child(obj.id);
-
-        newRef.once("value",function(snapshot){
-            console.log(snapshot.val());
-            res.json(snapshot.val());
-        });
+	req.on("end", () => {
+        var data = JSON.parse(body);
+        callBack(data);
     });
-});
+}
 
+var ERR = "ERROR";
+var SUC = "SUCCESS";
 
+function resp(res, type, body)
+{
+    res.json({
+        type: type,
+        body: body
+    }); 
+    console.log(type + ": " + body);
+}
 
-// Create web server
-http.createServer(app).listen(80, function(){
-	console.log("The server has been opened on port 80");
-});
+function checkData(res, data, args)
+{
+    console.log(data);
+    for(var x = 0; x < args.length; x++)
+        if(!data.hasOwnProperty(args[x]))
+        {
+            resp(res, ERR, "ARGUMENT [" + args[x] + "] MISSING");
+            return false;
+        }
+    return true;
+}
 
 // Random functions
 function genChars(amt){
@@ -247,15 +219,4 @@ function genChars(amt){
         str += Alpha.charAt(Math.random()*36);
     }
     return str;
-}
-
-var ERR = "ERROR";
-var RSP  = "RESPONSE";
-
-function resp(res, type, body)
-{
-    res.json({
-        type: type,
-        body: body
-    });
 }
